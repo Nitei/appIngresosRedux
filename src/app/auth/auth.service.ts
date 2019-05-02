f6aa024f78ef17d822d56104f848b4ef84f71ca9
Jorge <jorgecampoy81@gmail.com>
2019-05-02 20:41:14 +0200

    Agregado: crear usuarios, login y logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,25 +1,56 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 import Swal from 'sweetalert2';
+import { User } from './user.model';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private af: AngularFireAuth, private router: Router) { }
+  constructor(private af: AngularFireAuth, private router: Router, private afDb: AngularFirestore) { }
+
+  isAuth() {
+    return this.af.authState
+      .pipe(
+        map( fbUser => {
+          if (fbUser == null) {
+            this.router.navigate(['/login']);
+          }
+          return fbUser != null;
+          })
+      );
+  }
+
+  initAuthListener() {
+    this.af.authState.subscribe(fbUser => {
+      console.log(fbUser);
+    });
+  }
 
   crearUsuario(nombre: string, email: string, password: string): any {
     this.af.auth
       .createUserWithEmailAndPassword(email, password)
       .then( resp => {
-        this.router.navigate(['dashboard']);
-        console.log(resp);
+        const user: User = {
+          uid: resp.user.uid,
+          nombre: nombre,
+          email: resp.user.email
+        };
+
+        this.afDb.doc(`${user}/usuario`)
+          .set(user)
+            .then(() => {
+              this.router.navigate(['dashboard']);
+            })
+            .catch(error => console.error(error));
       })
       .catch((error: any) => {
       console.error(error);
-      Swal.fire('Error en el login', error['message'], 'error')
+      Swal.fire('Error en el login', error['message'], 'error');
     });
   }
 
@@ -32,8 +63,13 @@ export class AuthService {
     })
     .catch((error: any) => {
       console.error(error);
-      Swal.fire('Error en el login', error['message'], 'error')
+      Swal.fire('Error en el login', error['message'], 'error');
     });
+  }
+
+  logout() {
+    this.router.navigate(['/login']);
+    this.af.auth.signOut();
   }
 
 
