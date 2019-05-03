19aaa09561ed16fb9593ba9f3791366ca6ee9a07
Jorge <jorgecampoy81@gmail.com>
2019-05-03 11:04:57 +0200

    Agregado reducer y action de auth a el app.reducer

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,10 +1,14 @@
 import * as UI from './shared/ui.reducer';
+import * as Auth from './auth/auth.reducer';
+
 import { ActionReducerMap } from '@ngrx/store';
 
 export const appReducers: ActionReducerMap<AppState> = {
-    ui: UI.uiReducer
+    ui: UI.uiReducer,
+    auth: Auth.authReducer
 };
 
 export interface AppState {
     ui: UI.State;
+    auth: Auth.AuthState;
 }
