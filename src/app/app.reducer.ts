93f985e488f261c71122cc18b5859c6aadf613d0
Jorge <jorgecampoy81@gmail.com>
2019-05-05 19:44:00 +0200

    Agregadas action y reducer en app.reducer.ts

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,14 +1,18 @@
 import * as UI from './shared/ui.reducer';
 import * as Auth from './auth/auth.reducer';
+import * as IE from './ingreso-egreso/ingreso-egreso.reducer';
 
 import { ActionReducerMap } from '@ngrx/store';
+import { IngresoEgresoState } from './ingreso-egreso/ingreso-egreso.reducer';
 
 export const appReducers: ActionReducerMap<AppState> = {
     ui: UI.uiReducer,
-    auth: Auth.authReducer
+    auth: Auth.authReducer,
+    IngresoEgreso: IE.IngresoEgresoReducer
 };
 
 export interface AppState {
     ui: UI.State;
     auth: Auth.AuthState;
+    IngresoEgreso: IE.IngresoEgresoState;
 }
