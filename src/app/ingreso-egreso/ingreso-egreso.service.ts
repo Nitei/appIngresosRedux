99b2ac18334dc5b84f3cc245da84b59fed98e9c5
Jorge <jorgecampoy81@gmail.com>
2019-05-05 19:44:09 +0200

    cargados items de firebase

--- src/app/ingreso-egreso/ingreso-egreso.service.ts
+++ src/app/ingreso-egreso/ingreso-egreso.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { IngresoEgreso } from './ingreso-egreso.model';
 import { AuthService } from '../auth/auth.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +14,26 @@ export class IngresoEgresoService {
 
   constructor(
     private afDb: AngularFirestore,
-    private authService: AuthService
+    private authService: AuthService,
+    private store: Store<AppState>
     ) { }
+
+  initIngresoEgresoLitener() {
+    const user = this.authService.getUsuario();
+    this.store.select('auth')
+    .pipe(
+      filter(auth => auth.user != null)
+    )
+    .subscribe( auth => {
+      this.IngresoEgresoItems(auth.user.uid);
+    });
+  }
+
+  private IngresoEgresoItems(uid: string) {
+    this.afDb.collection(`${uid}/ingresos-egresos/items`)
+      .valueChanges()
+      .subscribe(data => console.log(data));
+  }
 
   crearIngresoEgreso(ingresoEgreso: IngresoEgreso) {
     const user = this.authService.getUsuario();
