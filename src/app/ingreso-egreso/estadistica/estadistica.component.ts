6396577df64b5e65b7434f4326f586cdcb0a1e44
Jorge <jorgecampoy81@gmail.com>
2019-05-07 00:21:42 +0200

    Agregada estadistica con chart.js

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -4,49 +4,48 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../../app.reducer';
 import { IngresoEgreso } from '../ingreso-egreso.model';
 import { subscribeOn } from 'rxjs/operators';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-estadistica',
   templateUrl: './estadistica.component.html',
-  styles: []
+  styles: [],
 })
 export class EstadisticaComponent implements OnInit {
-
   ingresos: number;
   egresos: number;
   cuantosIngresos: number;
   cuantosEgresos: number;
 
-  Subscription: Subscription = new Subscription;
+  public doughnutChartLabels: string[] = ['Ingresos', 'Egresos'];
+  public doughnutChartData: number[] = [0, 0];
+  public doughnutChartType = 'doughnut';
 
-  constructor(
-    private store: Store<AppState>,
-  ) { }
+  Subscription: Subscription = new Subscription();
+
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit() {
-    this.Subscription = this.store.select('IngresoEgreso')
-    .subscribe((ingresoEgreso: any) => {
+    this.Subscription = this.store.select('IngresoEgreso').subscribe((ingresoEgreso: any) => {
       this.contarIngresoEgresos(ingresoEgreso.items);
     });
   }
 
   contarIngresoEgresos(items: IngresoEgreso[]) {
+    this.ingresos = 0;
+    this.egresos = 0;
+    this.cuantosIngresos = 0;
+    this.cuantosEgresos = 0;
 
-  this.ingresos = 0;
-  this.egresos = 0;
-  this.cuantosIngresos = 0;
-  this.cuantosEgresos = 0;
-
-  items.forEach(element => {
-    if (element.tipo === 'ingreso') {
-      this.cuantosIngresos++;
-      this.ingresos += element.monto;
-    } else {
-      this.cuantosEgresos ++;
-      this.egresos += element.monto;
-    }
-  });
-
+    items.forEach(element => {
+      if (element.tipo === 'ingreso') {
+        this.cuantosIngresos++;
+        this.ingresos += element.monto;
+      } else {
+        this.cuantosEgresos++;
+        this.egresos += element.monto;
+      }
+    });
+    this.doughnutChartData = [this.ingresos, this.egresos];
   }
-
 }
