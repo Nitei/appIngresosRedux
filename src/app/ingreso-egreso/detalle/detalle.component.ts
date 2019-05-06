bdc62839422b76bbb748d865ba52610bb75bda01
Jorge <jorgecampoy81@gmail.com>
2019-05-06 09:54:47 +0200

    Agregada vista de detalles

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import { IngresoEgreso } from '../ingreso-egreso.model';
+import { filter } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { IngresoEgresoService } from '../ingreso-egreso.service';
 
 @Component({
   selector: 'app-detalle',
   templateUrl: './detalle.component.html',
   styles: []
 })
-export class DetalleComponent implements OnInit {
+export class DetalleComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  items: IngresoEgreso;
+  subsStoreIngresosEgresos: Subscription = new Subscription;
+
+  constructor(
+    private store: Store<AppState>,
+    public IES: IngresoEgresoService
+  ) { }
 
   ngOnInit() {
+    this.subsStoreIngresosEgresos = this.store.select('IngresoEgreso')
+      .pipe(
+        filter( (item: any) => item.items.length != 0) )
+        .subscribe( (ingresoEgreso: any) => {
+          this.items = ingresoEgreso.items;
+        });
+  }
+
+  ngOnDestroy() {
+    this.subsStoreIngresosEgresos.unsubscribe();
+  }
+
+  borrarItem(uid: string) {
+    this.IES.borrarIngresoEgreso(uid);
   }
 
 }
